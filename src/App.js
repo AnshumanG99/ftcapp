f236ff088062a0fedc6de9c24a384e53e1a1e749
Anshuman Garg <anshumangarg411@gmail.com>
2024-03-15 00:44:14 -0500

    Added support for draw function
    Created a draw function tool for both blue and red
    Awaiting bug fixes to prevent erasing of other markings when selecting a
    new color

--- src/App.js
+++ src/App.js
@@ -3,48 +3,47 @@ import img from './assets/img.png';
 import robot from './assets/robot.png';
 import './App.css';
 import { addListeners } from './moveable.js';
-import { addRobots, removeRobots } from './addRobot.js'
+import { addRobots, removeRobots } from './addRobot.js';
+import {addDrawListeners, deleteLine} from './draw.js';
+
 
 function App() {
     useEffect(() => {
-        addListeners(); // Call addListeners function when component mounts
+        addListeners();
+
     }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        FTC Game Planner
+      <header className="App-header"> FTC Game Planner </header>
+        <img src={img} className="FTCGameBoard" alt="board" draggable="false"/>
+        <img src={robot} className="Robot" id="bluerobot1" draggable="false" />
+        <img src={robot} className="Robot" id="bluerobot2" draggable="false" />
+        <img src={robot} className="Robot" id="redrobot1" draggable="false" />
+        <img src={robot} className="Robot" id="redrobot2" draggable="false" />
 
-        <p>
-          <img src={img} className="FTCGameBoard" alt="board" draggable="false" />
 
-          <img src={robot} className="Robot" id="bluerobot1" draggable="false" />
-          <img src={robot} className="Robot" id="bluerobot2" draggable="false" />
-          <img src={robot} className="Robot" id="redrobot1" draggable="false" />
-          <img src={robot} className="Robot" id="redrobot2" draggable="false" />
-       </p>
-
-          <div id="left-box" draggable = "false">
-          Blue Alliance Settings
-          <p>Blue Robots</p>
-          <button type="button" className = "BlueButtons" id = "blueRemoveRobot" onClick = {() => removeRobots('blue')}> Remove Robots </button>
-          <button type="button" className = "BlueButtons" id = "blueAddRobot" onClick = {() => addRobots('blue')}> Add Robots </button>
-          <p>Blue Drawing Tool</p>
-          <button type="button" className = "BlueButtons"> Blue Draw Tool </button>
-          <p>Blue Moving Tool</p>
-          <button type="button" className = "BlueButtons"> Move tool </button>
+        <div id="left-box" draggable = "false">
+            <h4> Blue Alliance Settings </h4>
+                <p>Blue Robots</p>
+                    <button type="button" className = "BlueButtons" id = "blueRemoveRobot" onClick = {() => removeRobots('blue')}> Remove Robots </button>
+                    <button type="button" className = "BlueButtons" id = "blueAddRobot" onClick = {() => addRobots('blue')}> Add Robots </button>
+                <p>Blue Drawing Tool</p>
+                    <button type="button" className = "BlueButtons" id = "" onClick = {() => addDrawListeners('blue')}> Blue Draw Tool </button>
+                <p>Blue Remove Tool</p>
+                    <button type="button" className = "BlueButtons" onClick = {() => deleteLine()}> Blue Remove tool </button>
         </div>
+
         <div id="right-box">
-          Red Alliance Settings
-          <p>Red Robots</p>
-          <button type="button" className = "RedButtons" id = "redRemoveRobot" onClick = {() => removeRobots('red')}> Remove Robots </button>
-          <button type="button" className = "RedButtons" id = "redAddRobot" onClick = {() => addRobots('red')}> Add Robots </button>
-          <p>Red Drawing Tool</p>
-          <button type="button" className = "RedButtons"> Red Draw Tool </button>
-          <p>Red Moving Tool</p>
-          <button type="button" className = "RedButtons"> Move tool </button>
+            <h4> Red Alliance Settings </h4>
+                <p>Red Robots</p>
+                    <button type="button" className = "RedButtons" id = "redRemoveRobot" onClick = {() => removeRobots('red')}> Remove Robots </button>
+                    <button type="button" className = "RedButtons" id = "redAddRobot" onClick = {() => addRobots('red')}> Add Robots </button>
+                <p>Red Drawing Tool</p>
+                    <button type="button" className = "RedButtons" onClick = {() => addDrawListeners('red')}> Red Draw Tool </button>
+                <p>Red Remove Tool</p>
+                    <button type="button" className = "RedButtons" onClick = {() => deleteLine()}> Red Remove Tool </button>
         </div>
-      </header>
     </div>
   );
 }
---
+++ src/draw.js
@@ -0,0 +1,133 @@
+let isDrawing = false;
+let startX = 0;
+let startY = 0;
+let currentColor = '';
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
+
+export function addDrawListeners(color){
+    currentColor = color;
+    const container = document.createElement('div');
+    const gameBoard = document.querySelector('.FTCGameBoard');
+    gameBoard.parentNode.insertBefore(container, gameBoard.nextSibling);
+
+    canvas.width = gameBoard.offsetWidth;
+    canvas.height = gameBoard.offsetHeight;
+    canvas.style.position = 'absolute';
+    canvas.style.top = '10vh';
+    canvas.style.left = '30vw';
+    container.appendChild(canvas);
+    window.addEventListener('mousedown', startDrawing);
+    window.addEventListener('mousemove', draw);
+    window.addEventListener('mouseup', stopDrawing);
+
+}
+
+
+function startDrawing(e){
+    isDrawing = true;
+    const rect = canvas.getBoundingClientRect();
+    startX = e.clientX -rect.left;
+    startY = e.clientY - rect.top;
+
+}
+
+function draw(e){
+
+    if (!isDrawing){
+        return;
+    }
+    e.stopPropagation();
+    e.preventDefault();
+    const rect = canvas.getBoundingClientRect();
+    ctx.strokeStyle = currentColor;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    startX = e.clientX - rect.left;
+    startY = e.clientY - rect.top;
+    ctx.lineTo(startX, startY);
+    ctx.stroke();
+}
+
+function stopDrawing(){
+    isDrawing = false;
+}
+
+export function deleteLine(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+}
+
+
+
+//
+//export function addDrawListeners(color){
+//    currentColor = color;
+//
+//    window.removeEventListener('mousedown', startDrawing);
+//    window.removeEventListener('mousemove', draw);
+//    window.removeEventListener('mouseup', stopDrawing);
+//
+//    console.log("current Color:", color);
+//    window.addEventListener('mousedown', startDrawing);
+//    window.addEventListener('mousemove', draw);
+//    window.addEventListener('mouseup', stopDrawing);
+//
+//    function startDrawing(e){
+//        isDrawing = true;
+//        startX = e.clientX;
+//        startY = e.clientY;
+//
+//    }
+//
+//    function draw(e){
+//        if (!isDrawing){
+//            return;
+//        }
+//        e.preventDefault();
+//        e.stopPropagation();
+//
+//        drawLine(startX, startY, e.clientX, e.clientY);
+//        startX = e.clientX;
+//        startY = e.clientY
+//
+//    }
+//
+//    function drawLine(x1, y1, x2, y2){
+//        let lineSegments = []
+//        const line = document.createElement('div');
+//        line.classList.add('line');
+//        line.style.position = 'absolute';
+//        line.style.width = '2px';
+//        line.style.height = '2px';
+//        console.log('Current Color:', currentColor);
+//        line.style.background = currentColor;
+//        line.style.top = `${y1}px`;
+//        line.style.left = `${x1}px`;
+//        line.style.zIndex = '9999';
+//        line.style.pointerEvents = 'none';
+//
+//        const length = Math.hypot(x2 -x1, y2 -y1);
+//        const angle = Math.atan2(y2-y1, x2-x1) * 180 / Math.PI;
+//        line.style.transform = `rotate(${angle}deg)`;
+//        line.style.width = `${length}px`;
+//
+//        document.body.appendChild(line);
+//        lineHistory.push(line);
+//    }
+//
+//    function stopDrawing(){
+//        isDrawing = false;
+//    }
+//}
+//
+//export function deleteLine(){
+//    if (lineHistory.length > 0){
+//        const lastLine = lineHistory.pop();
+//        lastLine.remove();
+//    }
+//}
